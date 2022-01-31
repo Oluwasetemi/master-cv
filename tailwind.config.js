d41381b7335a1d2c8f988dc776708050f2c33900
Ojo Oluwasetemi <setemiojo@gmail.com>
2022-01-31 14:33:15 +0100

    📦 NEW: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#966fdb',
+        text: '#181515',
+      },
+      fontFamily: {
+        sans: "'Josefin Sans', Ubuntu, sans-serif",
+        header: 'Lora, serif',
+      },
+    },
+  },
+  plugins: [],
+};
